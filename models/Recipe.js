39628b0cc577cf3e7f6a0d1fc68bee4dbef5c713
Habte Selassie <habtuchelsea1000@gmail.com>
2024-04-29 15:35:01 +0300

    create ed recipe db model

---
+++ models/Recipe.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  ingredients: [String],
+  instructions: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
