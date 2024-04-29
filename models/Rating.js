dd3a76882845051a347af93c83c162d99497e98c
Habte Selassie <habtuchelsea1000@gmail.com>
2024-04-29 15:45:40 +0300

    created a rating db model

---
+++ models/Rating.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const ratingSchema = new mongoose.Schema({
+  recipe: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Recipe',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Rating = mongoose.model('Rating', ratingSchema);
+
+module.exports = Rating;
